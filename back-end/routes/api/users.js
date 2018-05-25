8abcf4d53d3c87636ff386062f750d517e9b8619
Unknown <jasonl960523@gmail.com>
2018-05-25 15:51:33 +0800

    Create the Registration route

--- back-end/routes/api/users.js
+++ back-end/routes/api/users.js
@@ -4,6 +4,18 @@ var mongoose = require('mongoose'),
 var auth = require('../auth')
 var User = mongoose.model('User')
 
+// Registration
+router.post('/users', (req, res, next) => {
+  var user = new User()
 
+  user.username = req.body.user.username
+  user.setPassword(req.body.user.password)
+
+  user.save().then(() => {
+    return res.json({
+      user: user.toAuthJSON()
+    })
+  }).catch(next)
+})
 
 module.exports = router
