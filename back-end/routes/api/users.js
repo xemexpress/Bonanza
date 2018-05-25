5b18444442351778aa9223f98a859a8887ed9c95
Unknown <jasonl960523@gmail.com>
2018-05-25 16:15:58 +0800

    Create the Authentication route

--- back-end/routes/api/users.js
+++ back-end/routes/api/users.js
@@ -18,4 +18,28 @@ router.post('/users', (req, res, next) => {
   }).catch(next)
 })
 
+// Authentication
+router.post('/users/login', (req, res, next) => {
+  if(!req.body.user.username){
+    return res.status(422).json({ errors: { username: "can't be blank" } })
+  }
+
+  if(!req.body.user.password){
+    return res.status(422).json({ errors: { password: "can't be blank" } })
+  }
+
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    console.log('info:', info)
+    if(err){ return next(err) }
+
+    if(user){
+      return res.json({ user: user.toAuthJSON })
+    }else{
+      return res.status(422).json(info)
+    }
+
+    // possible delete for the else clause
+  })(req, res, next)
+})
+
 module.exports = router
