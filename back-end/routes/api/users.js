9b7cc7414f93f57c5ea9975cdf0f15ad7b698097
Unknown <jasonl960523@gmail.com>
2018-05-25 17:11:06 +0800

    Create the Update User route

--- back-end/routes/api/users.js
+++ back-end/routes/api/users.js
@@ -48,4 +48,27 @@ router.get('/user', auth.required, (req, res, next) => {
   }).catch(next)
 })
 
+// Update User
+router.put('/user', auth.required, (req, res, next) => {
+  User.findById(req.payload.id).then((user) => {
+    if(!user){ return res.sendStatus(401) }
+
+    if(typeof req.body.user.username !== 'undefined'){
+      user.username = req.body.user.username
+    }
+
+    if(typeof req.body.user.proPic !== 'undefined'){
+      user.proPic = req.body.user.proPic
+    }
+
+    if(typeof req.body.user.password !== 'undefined'){
+      user.setPassword(req.body.user.password)
+    }
+
+    return user.save().then((() => {
+      res.json({ user: user.toAuthJSON() })
+    }))
+  }).catch(next)
+})
+
 module.exports = router
