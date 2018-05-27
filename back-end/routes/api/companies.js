0b0ba5689cc31bf95288d778d5f4312b98ea0589
Unknown <jasonl960523@gmail.com>
2018-05-27 22:19:07 +0800

    Create the Create Company and Update Company routes

--- back-end/routes/api/companies.js
+++ back-end/routes/api/companies.js
@@ -2,5 +2,64 @@ var mongoose = require('mongoose'),
     router = require('express').Router()
 var auth = require('../auth')
 var Company = mongoose.model('Company')
+var User = mongoose.model('User')
+
+// Preload Company's symbol
+router.param('symbol', (req, res, next, symbol) => {
+  req.symbol = symbol
+
+  next()
+})
+
+// Create Company
+router.post('/', auth.required, (req, res, next) => {
+  User.findById(req.payload.id).then((user) => {
+    if(!user){ return res.sendStatus(401) }
+
+    // Should check if user already has the symbol.
+    if(true){
+      var company = new Company(req.body.company)
+    
+      company.author = user
+
+      return company.save().then(() => {
+        return res.json({ company: company.toJSONFor() })
+      })
+    }else{
+      return res.status(422).json({ errors: { 'company with the same symbol': 'already exists' } })
+    }
+  }).catch(next)
+})
+
+// Update Company
+router.put('/:symbol', auth.required, (req, res, next) => {
+  Company.findOne({ author: req.payload.id, symbol: req.symbol }).then((company) => {
+    if(!company){ return res.sendStatus(401) }
+
+    if(typeof req.body.company.symbol !== 'undefined'){
+      company.symbol = req.body.company.symbol
+    }
+
+    if(typeof req.body.company.name !== 'undefined'){
+      company.name = req.body.company.name
+    }
+
+    if(typeof req.body.company.abbr !== 'undefined'){
+      company.abbr = req.body.company.abbr
+    }
+
+    if(typeof req.body.company.logo !== 'undefined'){
+      company.logo = req.body.company.logo
+    }
+
+    if(typeof req.body.company.tagList !== 'undefined'){
+      company.tagList = req.body.company.tagList
+    }
+
+    return company.save().then(() => {
+      return res.json({ company: company.toJSONFor() })
+    })
+  }).catch(next)
+})
 
 module.exports = router
