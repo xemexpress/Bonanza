72a7a8a3f44b88df1015881429aea902e56679be
Unknown <jasonl960523@gmail.com>
2018-05-28 23:22:06 +0800

    Create the List, Add, Delete Record routes

--- back-end/routes/api/companies.js
+++ back-end/routes/api/companies.js
@@ -1,12 +1,20 @@
 var mongoose = require('mongoose'),
     router = require('express').Router()
 var auth = require('../auth')
-var Company = mongoose.model('Company')
-var User = mongoose.model('User')
+var User = mongoose.model('User'),
+    Company = mongoose.model('Company'),
+    Record = mongoose.model('Record')
 
 // Preload Company's symbol
 router.param('symbol', (req, res, next, symbol) => {
   req.symbol = symbol
+
+  next()
+})
+
+// Preload Record's year
+router.param('year', (req, res, next, year) => {
+  req.year = year
 
   next()
 })
@@ -67,6 +75,68 @@ router.delete('/:symbol', auth.required, (req, res, next) => {
   Company.remove({ author: req.payload.id, symbol: req.symbol }).then((deleted, err, what) => {
     return res.sendStatus(204)
   })
+})
+
+// Get Records from a Company
+router.get('/:symbol/records', auth.required, (req, res, next) => {
+  Company.findOne({ author: req.payload.id, symbol: req.symbol })
+    .populate({
+      path: 'records',
+      options: {
+        sort: { year: 1 }
+      }
+    })
+    .then((company) => {
+      if(!company){ return res.sendStatus(401) }
+      
+      return res.json({
+        records: company.records.map((record) => {
+          return record.toJSONFor()
+        }),
+        recordsCount: company.records.length
+      })
+    }).catch(next)
+})
+
+// Add Record to a Company
+router.post('/:symbol/records', auth.required, (req, res, next) => {
+  Company.findOne({ author: req.payload.id, symbol: req.symbol })
+    .populate('records', 'year')
+    .then((company) => {
+      if(!company){ return res.sendStatus(401) }
+      console.log('records:',company.records)
+      if(company.records.every((record) => record.year !== req.body.record.year)){
+        var record = new Record(req.body.record)
+        record.forCompany = company
+
+        return record.save().then(() => {
+          company.records.push(record)
+
+          return company.save().then(() => {
+            return res.json({ record: record.toJSONFor() })
+          })
+        })
+      }else{
+        return res.status(422).json({ errors: { 'year': "can't be repeated" } })
+      }
+    }).catch(next)
+})
+
+// Delete Record
+router.delete('/:symbol/records/:year', auth.required, (req, res, next) => {
+  Company.findOne({ author: req.payload.id, symbol: req.symbol })
+    .populate('records', 'year')
+    .then((company) => {
+      if(!company){ return res.sendStatus(401) }
+      
+      let recordId = company.records.find((record) => record.year === req.year)._id
+      company.records.remove(recordId)
+      company.save().then(() => {
+        Record.remove({ _id: recordId }).then(() => {
+          return res.sendStatus(204)
+        })
+      })
+    }).catch(next)
 })
 
 module.exports = router
