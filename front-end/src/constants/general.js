732a1c007d5e369ca8695b6a42daadf9751913de
Unknown <jasonl960523@gmail.com>
2018-10-17 22:50:23 +0800

    Updates for credentials

--- front-end/src/constants/general.js
+++ front-end/src/constants/general.js
@@ -5,7 +5,6 @@ export const THE_TAGS = ['Resonate', 'Grow', 'Belong', 'Ease', 'Live', 'Approach
 
 // Externals
 export const ALPHAVANTAHE_API_ROOT = 'https://www.alphavantage.co'
-export const ALPHAVANTAHE_API_KEY = '2B99AAK7MYLR7ZSV'
 
 // Display-related
 export const ARTICLES_PER_PAGE = 4
--- front-end/src/constants/index.js
+++ front-end/src/constants/index.js
@@ -2,3 +2,6 @@ export * from './general'
 export * from './images'
 export * from './actionTypes'
 export * from './sodium'
+
+// Required: ALPHAVANTAHE_API_KEY
+export * from './credentials'
